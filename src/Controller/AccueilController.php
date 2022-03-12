817cd5db150e04ef584c7e04308e24d5bca191e4
manel walha <manel.walha@esen.tn>
2022-03-12 12:41:38 +0100

    affichage questions

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Question;
+use App\Repository\QuestionRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class AccueilController extends AbstractController
 {
     /**
      * @Route("/", name="accueil")
      */
-    public function index(): Response
-    {
-        return $this->render('base.html.twig');
+    public function index(EntityManagerInterface $manager): Response
+    {   $QR=$manager->getRepository(Question::class);
+        $q=$QR->findAll();
+        return $this->render('accueil/index.html.twig',['questions'=>$q]);
+
     }
 }
